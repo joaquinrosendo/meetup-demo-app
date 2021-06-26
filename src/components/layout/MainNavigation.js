d3275099df11154888df87f2e47452b0010cd7bb
Joaquin Rosendo <joaquinrosendo@live.com>
2021-06-25 19:53:02 -0600

    added navigation w/ links

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+
+function MainNavigation() {
+    return (
+        <header>
+            <div>React Meetups</div>
+            <ul>
+                <li>
+                    <Link to='/'>All Meetups</Link>
+                </li>
+                <li>
+                    <Link to='/new-meetup'>New Meetups</Link>
+                </li>
+                <li>
+                    <Link to='/favorites'>Favorites</Link>
+                </li>
+            </ul>
+        </header>
+    );
+}
+
+export default MainNavigation;
