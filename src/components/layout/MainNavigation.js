5cb1b9e7960eefc6a3a50df77e57d88c0556d01e
Joaquin Rosendo <joaquinrosendo@live.com>
2021-06-25 20:05:39 -0600

    added css styles to MainNavigation component

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,9 +1,12 @@
 import { Link } from 'react-router-dom';
+// classes is made up object name, this object will contain classes from css file as properties
+import classes from './MainNavigation.module.css';
+
 
 function MainNavigation() {
     return (
-        <header>
-            <div>React Meetups</div>
+        <header className={classes.header}>
+            <div className={classes.logo}>React Meetups</div>
             <ul>
                 <li>
                     <Link to='/'>All Meetups</Link>
